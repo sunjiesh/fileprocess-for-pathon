df551db4fc9c92aeb319848a72a3b0966cce7ebc
tomsun <sunjie310110@gmail.com>
2013-09-02 20:47:10 +0800

    add test view

--- fileprocess/urls.py
+++ fileprocess/urls.py
@@ -14,4 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    
+    #process app
+    url(r'^process/',include('fileprocess.process.urls')),
 )
