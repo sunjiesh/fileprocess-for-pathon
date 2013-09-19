edf2a2eabf38f6a3845c5a5578e6a53614fde5e3
tomsun <sunjie310110@gmail.com>
2013-09-19 20:37:18 +0800

    add get pic file

--- fileprocess/process/urls.py
+++ fileprocess/process/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, url
 
 import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
     url(r'^$', views.index, name='index'), 
-    url(r'filetopic$', views.filetopic, name='filetopic')
+    url(r'filetopic$', views.filetopic, name='filetopic'), 
+    url(r'getpic$', views.getPic, name='getpic')
 )
--- fileprocess/process/views.py
+++ fileprocess/process/views.py
@@ -27,4 +27,35 @@ def filetopic(request):
         resultJson["responseId"]=requestId
         print resultJson
         return HttpResponse(str(resultJson)) 
-    return HttpResponse("Hello, world. You're at the pic index.") 
+    return HttpResponse("Hello, world. You're at the pic index,Please use post method") 
+
+@csrf_exempt
+def getPic(request):
+    """Get Pic File"""
+    if request.method=='POST':
+        jsonData=request.raw_post_data 
+        print jsonData
+        jsonData=json.loads(jsonData)
+        requestId=jsonData["requestId"]
+        requestId=str(requestId)
+        fieldsData=jsonData["fields"]
+        picPath=fieldsData["picPath"]
+        if picPath!="":
+            try:
+                def readFile(fn, buf_size=262144):
+                    f = open(fn, "rb")
+                    while True:
+                        c = f.read(buf_size)
+                        if c:
+                            yield c
+                        else:
+                            break
+                    f.close()
+                response = HttpResponse(readFile(picPath), mimetype='application/octet-stream')
+                response['Content-Disposition'] = 'attachment; filename=%s' %picPath
+                return response
+            except Exception, e:
+                print e
+        
+    return HttpResponse("Hello, world. You're at the getPic ,Please use post method") 
+        
