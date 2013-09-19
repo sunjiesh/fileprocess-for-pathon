9148861fbeb82b8735c39d099a6936c26defcca4
tomsun <sunjie310110@gmail.com>
2013-09-19 10:41:31 +0800

    document update

--- docs/fileprocess-for-pathon.mm
+++ docs/fileprocess-for-pathon.mm
@@ -1,9 +1,12 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1378126476107" ID="ID_1441292948" MODIFIED="1378126507060" TEXT="fileprocess-for-pathon">
-<node CREATED="1378126517449" ID="ID_341778050" MODIFIED="1378126526537" POSITION="right" TEXT="process">
+<node CREATED="1378126517449" ID="ID_341778050" MODIFIED="1379558205220" POSITION="right" TEXT="process">
 <node CREATED="1378126529529" ID="ID_596307700" MODIFIED="1378126572399" TEXT="restful api"/>
 <node CREATED="1378126576753" ID="ID_973302595" MODIFIED="1378126639208" TEXT="doc to pdf to png"/>
+<node CREATED="1378126576753" ID="ID_859855788" MODIFIED="1379558228425" TEXT="docx to pdf to png"/>
+<node CREATED="1378126576753" ID="ID_1126898262" MODIFIED="1379558232137" TEXT="xls to pdf to png"/>
+<node CREATED="1378126576753" ID="ID_474647653" MODIFIED="1379558236657" TEXT="xlst to pdf to png"/>
 </node>
 </node>
 </map>
